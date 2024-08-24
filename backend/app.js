11e8e2088be8720974d2c8b4d593e199ec246eb4
vickysingh12 <72865000+vickysingh12@users.noreply.github.com>
2024-08-24 13:53:59 +0530

    remote 1

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
+let finallybuddy = "test";
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
